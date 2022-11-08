41a4c8132c13420be5720f7bfd479d5be45bef33
skuettel <117479887+skuettel@users.noreply.github.com>
2022-11-08 13:23:02 +0100

    2 files modified, 7 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 import { MainComponent } from './main/main.component';
 
 import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, FlexModule],
-  declarations: [AppComponent, HelloComponent, MainComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MainComponent,
+    HeaderComponent,
+    ContentComponent,
+    SidebarComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
